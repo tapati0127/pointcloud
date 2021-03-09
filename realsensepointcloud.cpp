64f5d5ea1f64bbf658a97d63f3405595145a62ef
tapati0127 <phattrienthai99@gmail.com>
2021-03-09 23:14:00 +0700

    down sampling and normal estimation

---
+++ realsensepointcloud.cpp
@@ -0,0 +1,6 @@
+#include "realsensepointcloud.h"
+
+RealsensePointCloud::RealsensePointCloud()
+{
+
+}
---
+++ realsensepointcloud.h
@@ -0,0 +1,11 @@
+#ifndef REALSENSEPOINTCLOUD_H
+#define REALSENSEPOINTCLOUD_H
+
+
+class RealsensePointCloud
+{
+public:
+    RealsensePointCloud();
+};
+
+#endif // REALSENSEPOINTCLOUD_H
